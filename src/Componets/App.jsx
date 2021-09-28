2ee62f4889f47f53cd9dc437c5f93d2607ebbbaf
Igoris Ivanovas <igoris.x@inbox.lt>
2021-09-28 19:11:06 +0300

    header nav

--- src/Componets/App.jsx
+++ src/Componets/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import HeaderNav from './HeaderNav';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <HeaderNav />
     </div>
   );
 };
