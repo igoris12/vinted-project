5f3f93bca0bad9c3de324632e40366591b3540a2
Igoris Ivanovas <igoris.x@inbox.lt>
2021-10-17 19:18:47 +0300

    hero rework

--- src/Componets/Hero.jsx
+++ src/Componets/Hero.jsx
@@ -5,12 +5,19 @@ const Hero = ({ title, secondaryLinkText, primaryLinkText }) => {
     <>
       <div className="hero-container">
         <div className="hero-container-background">
-          <div className="card">
-            <h1 className="card-title">{title}</h1>
-            <div className="button-container">
-              <a className="but but-primary">{primaryLinkText}</a>
-              <a className="but but-noBorder">{secondaryLinkText}</a>
-            </div>
+          <div className="img-container">
+            <img
+              src="https://www.vinted.lt/assets/seller-promotion/other/banner_tablets_up-afe3d19776592a72f165c1bb93fd02c5528250a8c670ecc1656654323f9d4856.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="card">
+          <h1 className="card-title">{title}</h1>
+
+          <div className="button-container">
+            <a className="but but-primary">{primaryLinkText}</a>
+            <a className="but but-noBorder">{secondaryLinkText}</a>
           </div>
         </div>
       </div>
