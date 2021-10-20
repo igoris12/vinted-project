699f1462d58801901a61b5f14e98fa4000829227
Igoris Ivanovas <igoris.x@inbox.lt>
2021-10-20 19:08:41 +0300

    update ids featching

--- src/Componets/NewOffers.jsx
+++ src/Componets/NewOffers.jsx
@@ -12,32 +12,13 @@ const NewOffers = ({ headerText }) => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    getProductsIds('https://in3.dev/vinted/api/news/').then((data) =>
-      setProductsIds(data)
-    );
-    getProducts.then((data) => {
-      setProducts(data);
-    });
+    fetch('https://in3.dev/vinted/api/news/')
+      .then((response) => response.json())
+      .then((data) => setProductsIds(data));
   }, []);
 
-  const getProductsIds = (link) => {
-    return new Promise((resolve, reject) => {
-      fetch(link).then((response) => resolve(response.json()));
-    });
-  };
-
-  const getProducts = new Promise((resolve, reject) => {
-    let products = [];
-    productsIds.forEach((id) => {
-      fetch('https://in3.dev/vinted/api/products/' + id.id).then((response) =>
-        products.push(response.json())
-      );
-    });
-    resolve(products);
-  });
-
   console.log(productsIds);
-  console.log(products, 2);
+  // console.log(products, 2);
 
   return (
     <>
