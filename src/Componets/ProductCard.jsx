719e2c2be39883328fcb462708b533ffd1d308cd
Igoris Ivanovas <igoris.x@inbox.lt>
2021-10-22 18:21:07 +0300

    got img data

--- src/Componets/ProductCard.jsx
+++ src/Componets/ProductCard.jsx
@@ -3,40 +3,38 @@ import { useState, useEffect } from 'react';
 
 const ProductCard = ({ productId }) => {
   const [product, setProduct] = useState({});
+  const [main_img, setMain_img] = useState('#');
 
   useEffect(() => {
     fetch(`https://in3.dev/vinted/api/products/${productId}`)
       .then((response) => response.json())
       .then((data) => {
         setProduct(data);
+        setMain_img(data.img[data.main_img]);
       });
   }, []);
   console.log(product);
   return (
     <>
-      {product ? (
-        <div className={'ProductCard-container'}>
+      <div className={'ProductCard-container'}>
+        <div>
+          <i>ICON</i>
+          <p>name</p>
+        </div>
+        <div>
+          <img src={main_img} alt="Product Image" />
+        </div>
+        <div>
+          <b>
+            <p>{product.price + ' €'} </p>
+          </b>
+          <p>Size</p>
+          <p>color</p>
           <div>
-            <i>ICON</i>
-            <p>name</p>
-          </div>
-          <div>
-            <img src={'4'} alt="Product Image" />
-          </div>
-          <div>
-            <b>
-              <p>{product.price + ' €'} </p>
-            </b>
-            <p>Size</p>
-            <p>color</p>
-            <div>
-              <i>icon</i>likes
-            </div>
+            <i>icon</i>likes
           </div>
         </div>
-      ) : (
-        <></>
-      )}
+      </div>
     </>
   );
 };
