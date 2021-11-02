83f42fb00df4a4f57f13b6e903db7bb81900e62b
Igoris Ivanovas <igoris.x@inbox.lt>
2021-11-02 20:12:20 +0200

    get brands from api

---
+++ src/Componets/Brends.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
+const Brends = () => {
+  const [brands, setBrands] = useState([]);
+
+  useEffect(() => {
+    const brandsCopy = JSON.parse(localStorage.getItem('brands'));
+    if (brandsCopy == null) {
+      getBrands();
+    } else {
+      setBrands(brandsCopy);
+    }
+  }, []);
+  const getBrands = async () => {
+    try {
+      const data = await fetch('https://in3.dev/vinted/api/brands/all');
+      const brandsArray = await data.json();
+      setBrands(brandsArray);
+      localStorage.setItem('brands', JSON.stringify(brandsArray));
+    } catch (e) {
+      console.error('Sorry you are not lucky today.');
+    }
+  };
+
+  console.log(brands);
+  return <>Content</>;
+};
+
+export default Brends;
