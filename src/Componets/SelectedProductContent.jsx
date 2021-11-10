38c6b464af194f637a61fc236dab7dcba9057f47
Igoris Ivanovas <igoris.x@inbox.lt>
2021-11-10 20:11:43 +0200

    Make stylesheet for component

--- src/Componets/SelectedProductContent.jsx
+++ src/Componets/SelectedProductContent.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-function SelectedProductContent({ user, product }) {
+function SelectedProductContent({ user, product, brand }) {
+  console.log(product);
   return (
-    <>
+    <div className="product">
       <div className="product-container">
-        <div className="product gallery"></div>
-        <div className="product info">CONTENT</div>
+        <div className="product-gallery">Content</div>
+        <div className="product-info">
+          <span className="product-price">{product.price + ' €'}</span>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
