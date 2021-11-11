40a98af52e9e2aa70055a77b44e8947df6438b8f
Igoris Ivanovas <igoris.x@inbox.lt>
2021-11-11 21:24:15 +0200

    Product info layout done

--- src/Componets/SelectedProductContent.jsx
+++ src/Componets/SelectedProductContent.jsx
@@ -1,30 +1,61 @@
 import React from 'react';
 
 function SelectedProductContent({ user, product, brand }) {
+  console.log(user);
   return (
     <div className="product">
       <div className="product-container">
         <div className="product-gallery">Content</div>
         <div className="product-info">
-          <span className="product-price">{product.price + ' €'}</span>
-          <div className="brand">
-            <p className="title">PREKĖS ŽENKLAS</p>
-            <a className="value" href="#">
-              {brand.title}
-            </a>
+          <div className="column-1">
+            <span className="product-price">{product.price + ' €'}</span>
+            <div className="brand">
+              <p className="title">PREKĖS ŽENKLAS</p>
+              <a className="value" href="#">
+                {brand.title}
+              </a>
+            </div>
+            <div className="size">
+              <p className="title">DYDIS</p>
+              <p className="value">
+                {product.size &&
+                  product.size.map((size, index) => {
+                    if (index < product.size.length - 1) {
+                      return size + ' / ';
+                    } else {
+                      return size;
+                    }
+                  })}
+              </p>
+            </div>
+            <div className="size">
+              <p className="title">ĮKELTA</p>
+              <p className="value">
+                {'PRIEŠ ' + Math.round(Math.random() * 59) + ' MINUTES'}
+              </p>
+            </div>
           </div>
-          <div className="size">
-            <p className="title">DYDIS</p>
-            <p className="value">
-              {product.size &&
-                product.size.map((size, index) => {
-                  if (index < product.size.length - 1) {
-                    return size + ' / ';
-                  } else {
-                    return size;
-                  }
-                })}
-            </p>
+
+          <div className="column-2">
+            <div className="product-description">
+              <span className="title">{product.title}</span>
+              <p>{product.desc}</p>
+              <div className="but-container">
+                <button type="button">Teirautis pardavėjo</button>
+                <button type="button">Pažymėti</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="column-3">
+            <div className="user-info">
+              <label>
+                <div className="user-img">
+                  <img src={user.avatar} />
+                </div>
+                <p className="user-name">{user.name}</p>
+              </label>
+            </div>
           </div>
         </div>
       </div>
