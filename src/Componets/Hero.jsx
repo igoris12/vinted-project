7bc0ec7c8d26c3324c945580395e739817f63c57
Igoris Ivanovas <igoris.x@inbox.lt>
2021-11-26 20:53:36 +0200

    hero image effect added

--- src/Componets/Hero.jsx
+++ src/Componets/Hero.jsx
@@ -16,6 +16,7 @@ const Hero = ({
               src="https://www.vinted.lt/assets/seller-promotion/other/banner_tablets_up-afe3d19776592a72f165c1bb93fd02c5528250a8c670ecc1656654323f9d4856.jpg"
               alt="background img"
             />
+            <div className="img-effect"></div>
           </div>
         </div>
         <div className="card">
